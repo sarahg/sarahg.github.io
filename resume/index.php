4bfde9d3dc72118a87d57cd101cb08b3b616dde2
Sarah German <sarah@advomatic.com>
2017-01-30 17:10:49 -0600

    Import existing site files

---
+++ resume/index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<!--[if IE 7]> <html class="ie7 ie" lang="en"> <![endif]-->
+<!--[if IE 8]> <html class="ie8 ie" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+
+  <title>Resume | Sarah German</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="author" content="Sarah German">
+  <link href='http://fonts.googleapis.com/css?family=Candal' rel='stylesheet' type='text/css'>
+  <link href="../css/resume.css" rel="stylesheet">
+  <!--[if lt IE 9]>
+  <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script>
+  $(function(){
+    // print button
+    $(".print").bind("click", function(){
+      window.print();
+    });
+  });
+</script>
+</head>
+
+<body id="resume">
+
+      <section class="cf">
+
+        <header>
+            <div id="mynameis">
+              <h1 id="germano"><a href="/">Sarah German</a></h1>
+              <h2>Web Developer &amp; UX Designer</h2>
+            </div>
+        </header>
+
+        <section>
+            <article>
+                <h2>Experience</h2>
+
+                <span class="job-daterange">July 2014 - present</span>
+                <h3><a href="https://www.advomatic.com">Advomatic</a>
+                </h3>
+                <h4>Developer</h4>
+                <p>
+                  Agile Drupal development for large progressive advocacy organizations.
+                  Custom module development and advanced site building using modern web technologies.
+                </p>
+
+                <span class="job-daterange">November 2013 - July 2014</span>
+                <h3><a href="http://www.publicradio.org">American Public Media | Minnesota Public Radio</a></h3>
+                <h4>Web Developer</h4>
+                <p>
+                  Back-end Drupal development for the public radio program <a href="http://www.marketplace.org"><em>Marketplace</em></a>.
+                  Architected CMS-driven solutions for multimedia content creation and syndication. Continuously improved site performance.
+                </p>
+
+                <span class="job-daterange">September 2009 - November 2013</span>
+                <h3><a href="http://www.smm.org">Science Museum of Minnesota</a></h3>
+                <h4>Multimedia Designer</h4>
+                <p>
+                    Designed and developed hands-on exhibits, data visualizations, and web experiences
+                    using Drupal and related web technologies.
+                </p>
+
+                <span class="job-daterange">June 2008 - September 2009</span>
+                <h3><a href="http://www.augustash.com">August Ash</a></h3>
+                <h4>Client Services Manager/Developer</h4>
+                <p>
+                  Managed ongoing technical needs of 250+ clients, including improving websites,
+                  technical troubleshooting, training and support.
+                </p>
+
+            </article>
+            <article>
+                <h2>Education</h2>
+                <h3><a href="http://www.du.edu">University of Denver</a></h3>
+                <p>
+                    B.A. <a href="http://www.du.edu/ahss/edp/degree/ba.html">Digital Media Studies</a>
+                    <br />
+                    Graduated June 2007 | Minors: Geography, Sociology
+                </p>
+            </article>
+        </section>
+        <aside>
+          <h2>Proficiencies</h2>
+            <dl>
+              <dt>Languages</dt>
+                <dd>PHP</dd>
+                <dd>SQL</dd>
+                <dd>JavaScript</dd>
+                <dd>XML/XSLT/JSON</dd>
+                <dd>HTML5</dd>
+                <dd>CSS3</dd>
+            </dl>
+            <dl>
+              <dt>Tools</dt>
+                <dd>Drupal</dd>
+                <dd>WordPress</dd>
+                <dd>Git, SVN</dd>
+                <dd>jQuery</dd>
+                <dd>Grunt, SASS, Compass</dd>
+                <dd>LAMP stack</dd>
+                <dd>Varnish/Redis</dd>
+                <dd>Apache Solr</dd>
+                <dd>Adobe Creative Suite</dd>
+                <dd>Google Analytics</dd>
+            </dl>
+            <dl>
+              <dt>Skills</dt>
+                <dd>Responsive web design</dd>
+                <dd>User experience</dd>
+                <dd>Accessibility</dd>
+                <dd>UNIX command line</dd>
+                <dd>Shell scripting</dd>
+                <dd>Data visualization</dd>
+                <dd>Layout and wireframing</dd>
+                <dd>Technical documentation</dd>
+                <dd>Blogging</dd>
+                <dd>Data migration</dd>
+            </dl>
+        </aside>
+
+    </section>
+
+  <p id="next-steps"><span class="btn print">Print this Page</span></p>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-37083906-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</body>
+</html>
+

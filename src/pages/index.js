d967d5dff454dd7b97d5c8560d7ed5d171f88b7a
Sarah German <sgerman@pantheon.io>
2019-04-13 15:27:51 -0500

    Install Gatsy and start marking up pages.

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default () => (
+  <div>
+    <h1>Sarah German</h1>
+    <h2>Internet fixer / Beer maker / Minneapolitan</h2>
+    <p>I manage the US-East support team at <a href="https://www.pantheon.io">Pantheon</a>, where we help people build and run Drupal and WordPress websites on our cloud-based <a href="https://pantheon.io/platform/website-management-platform">website management platform</a>.</p>
+    <p>Outside of work, I make <a href="https://www.catpartybrewing.party">beer</a>, play some instruments, XC ski, and hang out with my cats.</p>
+    <div>
+      <i class="e1a-metal_tone2 e1a-2x"></i>
+      <Link to="/resume/">Check out my resume</Link>
+    </div>
+  </div>
+)
---
+++ src/pages/resume.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default () => (
+  <div>
+    <h1>wow much react</h1>
+  </div>
+)

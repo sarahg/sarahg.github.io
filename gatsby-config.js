13cbc02b564efebee95606bd541409c8feda645a
Sarah German <sgerman@pantheon.io>
2019-04-22 15:58:10 -0500

    Set up deploy to GitHub

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,4 @@
 module.exports = {
-    siteMetadata: {
-        title: `Gatsby`,
-        siteUrl: `https://www.gatsbyjs.org`,
-        description: `Blazing fast modern site generator for React`,
-    },
     plugins: [
         "gatsby-transformer-json",
         {

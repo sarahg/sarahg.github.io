73eaef641cc84504be57bc669ffc9e8cafee3bc9
Sarah German <sgerman@pantheon.io>
2020-02-14 09:05:37 -0600

    Remove unused var

--- src/pages/about-this-site.js
+++ src/pages/about-this-site.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-import { Link } from "gatsby"
 import Header from "../components/header"
 import Footer from "../components/footer"
 

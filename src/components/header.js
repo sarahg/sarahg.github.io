6f1f9d1f56afb4603ecb2546e9f08356ab7c0dfb
Sarah German <sarah@clockwork.com>
2021-11-27 13:45:00 -0600

    Update resume

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 
 function headerText(props) {
-    return <h1><Link to="/">{props.headerText}</Link></h1>
+    return <h1><Link to="https://sarahgerman.com">{props.headerText}</Link></h1>
 }
 
 export default headerText
